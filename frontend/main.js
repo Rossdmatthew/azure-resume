dce59b387ff4815b379916a004dd64a86069107a
Rossdmatthew <146154563+Rossdmatthew@users.noreply.github.com>
2024-03-06 19:11:50 -0600

    Place Bin folder in root directory.

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
         getVisitCount();
-})
-const functionApiUrl = 'https://getvisitorcounterf.azurewebsites.net'
-const localApi = 'http://localhost:7071/api/GetVisitorCounterF';
-
-const getVisitCount = () => {
-    let count = 30
-    fetch(localApi).then(response => {
-            return response.json ()
-    }).then(response =>{
+    })
+    
+    const productionApiUrl = 'https://getvisitorcountf.azurewebsites.net/api/getvisitorcounterf'
+    const localApiUrl = 'http://localhost:7071/api/GetVisitorCounterF';
+    
+    const getVisitCount = () => {
+        let count = 30;
+        fetch(productionApiUrl).then(response => {
+            return response.json()
+        }).then(response =>{
             console.log("Website called function API.");
             count =  response.count;
             document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    });
-    return count;
-}
+        }).catch(function(error){
+            console.log(error);
+        });
+        return count;
+    }
