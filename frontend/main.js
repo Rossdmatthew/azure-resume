4429ff002cb4b383bea37e0d1b0636c75916696a
Rossdmatthew <146154563+Rossdmatthew@users.noreply.github.com>
2024-03-13 21:27:44 -0500

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         getVisitCount();
     })
     
-    const productionApiUrl = 'https://getvisitorcountf.azurewebsites.net/api/getvisitorcounterf'
+    const productionApiUrl = 'https://getvisitorcounterf.azurewebsites.net/'
     const localApiUrl = 'http://localhost:7071/api/GetVisitorCounterF';
     
     const getVisitCount = () => {
